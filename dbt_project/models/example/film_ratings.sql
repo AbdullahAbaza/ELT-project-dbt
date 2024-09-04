c5c15d154234d2b0f9c0d488fbce5c1590b7e90c
AbdullahAbaza <abdullahabaza96@gmail.com>
2024-09-04 07:44:24 -0400

    Add dbt models for film ratings and film actors
    This commit adds new dbt models for film ratings and film actors. The film_ratings.sql file defines a CTE (common table expression) that categorizes user ratings into "Excellent", "Good", "Average", or "Poor". The film_actors.sql file aggregates the actors for each film using a LEFT JOIN. These models provide additional insights into the films dataset.

---
+++ dbt_project/models/example/film_ratings.sql
@@ -0,0 +1,36 @@
+WITH films_with_ratings AS (
+    SELECT
+        film_id,
+        title,
+        release_date,
+        price,
+        rating,
+        user_rating
+        CASE 
+            WHEN user_rating >= 4.5 THEN "Excellent"
+            WHEN user_rating >= 3.5 THEN "Good"
+            WHEN user_rating >= 2.5 THEN "Average"
+            ELSE "Poor"
+            END AS rating_category
+    FROM {{ ref('films') }}
+),
+
+
+WITH films_with_actors AS (
+    SELECT
+        f.film_id,
+        f.title,
+        STRING_AGG(a.actor_name, ', ') AS actors
+
+    FROM {{ ref('films') }} AS f 
+    LEFT JOIN {{ ref('film_actors') }} AS fa ON f.film_id = fa.film_id 
+    LEFT JOIN {{ ref('actors')}} as a ON a.actor_id = fa.actor_id
+    GROUP BY f.film_id, f.title
+)
+
+
+SELECT
+    fwr.*,
+    fwa.actors
+FROM file_with_ratings AS fwr
+JOIN films_with_actors AS fwa ON fwr.film_id = fwa.film_id
